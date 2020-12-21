7459f8e8ac4ad433c35dd80ff4a552cda706cee4
YTG123 <54103478+YTG1234@users.noreply.github.com>
2020-12-21 08:41:42 +0200

    No need for sync

--- src/main/java/io/github/ytg1234/recipeconditions/api/RecipeConds.java
+++ src/main/java/io/github/ytg1234/recipeconditions/api/RecipeConds.java
@@ -3,7 +3,6 @@ package io.github.ytg1234.recipeconditions.api;
 import io.github.ytg1234.recipeconditions.RecipeCondsConstants;
 import io.github.ytg1234.recipeconditions.api.condition.RecipeCondition;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
-import net.fabricmc.fabric.api.event.registry.RegistryAttribute;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,9 +11,7 @@ public final class RecipeConds {
             RECIPE_CONDITION =
             FabricRegistryBuilder.createSimple(RecipeCondition.class,
                     new Identifier(RecipeCondsConstants.MOD_ID, "recipe_condition")
-                                              )
-                    .attribute(RegistryAttribute.SYNCED)
-                    .buildAndRegister();
+                                              ).buildAndRegister();
 
     private RecipeConds() {
     }
