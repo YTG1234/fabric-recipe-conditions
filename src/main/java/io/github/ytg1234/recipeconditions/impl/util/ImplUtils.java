92dcce33af2ef909297d5f07e03fee74aef03941
YTG123 <54103478+YTG1234@users.noreply.github.com>
2020-12-30 14:41:55 +0200

    Now, we'll see what happens...

--- src/main/java/io/github/ytg1234/recipeconditions/impl/util/ImplUtils.java
+++ src/main/java/io/github/ytg1234/recipeconditions/impl/util/ImplUtils.java
@@ -1,9 +1,11 @@
 package io.github.ytg1234.recipeconditions.impl.util;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import io.github.ytg1234.recipeconditions.RecipeCondsConstants;
 import io.github.ytg1234.recipeconditions.api.condition.AnyCondition;
@@ -19,8 +21,17 @@ import net.fabricmc.loader.api.VersionParsingException;
 import net.fabricmc.loader.util.version.VersionPredicateParser;
 
 public final class ImplUtils {
-    private static final Logger logger = LogManager.getLogger(ImplUtils.class);
+    private static final String CONDITIONS_MEMBER = "frc:conditions";
+    private static final Logger logger = LogManager.getLogger("Recipe Conditions");
 
+    /**
+     * Checks if a mod is loaded and matches a specific version range.
+     *
+     * @param id              the id of the mod to be checked
+     * @param requiredVersion the version or range to check for
+     *
+     * @return whether the requirement was satisfied
+     */
     public static boolean modVersionLoaded(String id, String requiredVersion) {
         Optional<ModContainer> mod = FabricLoader.getInstance().getModContainer(id);
         if (mod.isPresent()) {
@@ -35,6 +46,32 @@ public final class ImplUtils {
         return false;
     }
 
+    public static void tryLoadRecipes(Map<Identifier, JsonElement> map) {
+        Iterator<Map.Entry<Identifier, JsonElement>> recipes = map.entrySet().iterator(); // Used to avoid a CME
+
+        while (recipes.hasNext()) {
+            Map.Entry<Identifier, JsonElement> entry = recipes.next();
+            logger.info("Attempting to load recipe " + entry.getKey().toString());
+
+            checkForForgeConditionRecipe(entry);
+
+            try {
+                if (shouldRemoveRecipe(entry)) recipes.remove();
+            } catch (JsonParseException e) {
+                throw new JsonParseException("Failed to load recipe " + entry.getKey().toString() + "!", e);
+            }
+        }
+    }
+
+    private static void checkForForgeConditionRecipe(Map.Entry<Identifier, JsonElement> recipe) {
+        if (!recipe.getValue().isJsonObject()) return;
+        if (recipeUsesForgeStyleConditions(recipe.getKey(), recipe.getValue().getAsJsonObject())) {
+            logger.warn("Recipe " +
+                        recipe.getKey().toString() +
+                        " is using a \"conditions\" property and is not a Forge mod. Consider switching to FRC?");
+        }
+    }
+
     /**
      * Checks a recipe for conditions, processes them and decides whether to remove the recipe.
      *
@@ -44,21 +81,47 @@ public final class ImplUtils {
      *
      * @throws JsonParseException if the recipe has a conditions property but parsing it failed
      */
-    public static boolean shouldRemoveRecipe(Map.Entry<Identifier, JsonElement> entry, String conditionsMember) throws JsonParseException {
-        logger.info("Attempting to load recipe " + entry.getKey().toString());
-
+    private static boolean shouldRemoveRecipe(Map.Entry<Identifier, JsonElement> entry) throws JsonParseException {
         JsonElement recipe = entry.getValue();
 
         if (!recipe.isJsonObject()) return false;
-        if (!recipe.getAsJsonObject().has(conditionsMember)) return false;
+        if (!recipe.getAsJsonObject().has(ImplUtils.CONDITIONS_MEMBER)) return false;
 
-        logger.debug("Recipe " + entry.getKey().toString() + " has a " + conditionsMember + " property.");
-        AnyCondition conditions = AnyCondition.fromJson(recipe.getAsJsonObject().get(conditionsMember).getAsJsonArray());
+        logger.debug("Recipe " + entry.getKey().toString() + " has a " + ImplUtils.CONDITIONS_MEMBER + " property.");
+        AnyCondition conditions = AnyCondition.fromJson(recipe.getAsJsonObject().get(ImplUtils.CONDITIONS_MEMBER).getAsJsonArray());
         if (!conditions.check()) {
             logger.debug("Conditions didn't match, removing recipe " + entry.getKey().toString());
             return true;
         }
 
         return false;
+    }
+
+    /**
+     * Checks if a recipe has a {@code "conditions"} parameter and was
+     * not patched using Patchwork.
+     *
+     * @param recipe the ID to check, body won't be enough
+     * @param body   the recipe {@link JsonObject}
+     *
+     * @return whether the recipe belongs to a non-Forge mod and is still using the Forge-style conditions
+     */
+    private static boolean recipeUsesForgeStyleConditions(Identifier recipe, JsonObject body) {
+        return body.has("conditions") && !isForgeMod(recipe.getNamespace());
+    }
+
+    /**
+     * Checks if a mod of a certain ID
+     * was patched using Patchwork.
+     *
+     * @param id the id of the mod
+     *
+     * @return whether it was patched with Patchwork
+     */
+    private static boolean isForgeMod(String id) {
+        Optional<ModContainer> modAccess = FabricLoader.getInstance().getModContainer(id);
+        if (!modAccess.isPresent()) return false;
+        ModContainer mod = modAccess.get();
+        return mod.getMetadata().containsCustomValue("patchwork:patcherMeta");
     }
 }
